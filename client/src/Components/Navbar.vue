ad08fc75c9000dd45e038d7da99396d01d4c3feb
prabi manandhar <prabinmanandhar20@gmail.com>
2020-02-10 07:55:07 +0545

    vue frontend setup only

---
+++ client/src/Components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+
+  </div>
+</template>
+<script>
+
+export default {
+    components:{
+    }
+}
+</script>
+<style scoped>
+.navbar{
+    width: 100%;
+    height: 70px;
+    border-radius: 5px;
+    background-color: rgb(248, 240, 196)
+}
+
+.navbar:hover{
+      transform: scale(1.005);
+}
+
+</style>
---
+++ client/src/pages/PageHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <Navbar />
+</template>
+
+
+<script>
+import Navbar from '../Components/Navbar'
+
+  export default {
+    components:{
+      Navbar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
